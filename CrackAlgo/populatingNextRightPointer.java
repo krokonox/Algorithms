506ea0154477c40a5732eed10f57bc17e800ef31
krokonox <kuricyna20@Gmail.com>
2022-04-08 08:55:47 +0200

    Update populatingNextRightPointer.java

--- CrackAlgo/populatingNextRightPointer.java
+++ CrackAlgo/populatingNextRightPointer.java
@@ -73,3 +73,18 @@ class Solution {
     }
     
 }
+
+// DFS recursive
+
+class Solution {
+    
+    public Node connect(Node root) {
+        if(root == null) return null;
+        if(root.left != null) root.left.next = root.right;
+        if(root.right != null && root.next != null) root.right.next = root.next.left;
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+
+}
