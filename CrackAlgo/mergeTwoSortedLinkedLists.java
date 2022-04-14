fc0ef71461b1b4274926fb673638cdb74a902b3a
krokonox <kuricyna20@Gmail.com>
2022-04-14 20:44:31 +0200

    Create mergeTwoSortedLinkedLists.java

---
+++ CrackAlgo/mergeTwoSortedLinkedLists.java
@@ -0,0 +1,40 @@
+// Leetcode 21
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+* Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode newList = dummy;
+        
+        while(list1 != null && list2 != null) {
+            if(list1.val > list2.val) {
+                dummy.next = list2;
+                list2 = list2.next;
+            } else {
+                dummy.next = list1;
+                list1 = list1.next;
+            }
+            
+            dummy = dummy.next;
+        }
+        
+        if(list1 != null) {
+            dummy.next = list1;
+        } else {
+            dummy.next = list2;
+        }
+        
+        return newList.next;
+    }
+}
