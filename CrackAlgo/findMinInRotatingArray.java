9a1e49153c65ab75853fd637b070f73d2524f6a7
krokonox <kuricyna20@Gmail.com>
2022-04-23 17:57:20 +0200

    Create findMinInRotatingArray.java

---
+++ CrackAlgo/findMinInRotatingArray.java
@@ -0,0 +1,27 @@
+// Leetcode 153. Find Minimum in Rotated Sorted Array
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        if(nums[0] < nums[right]) return nums[0];
+        
+        while(right >= left) {
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] > nums[mid + 1]) return nums[mid + 1];
+            if(nums[mid - 1] > nums[mid + 1]) return nums[mid];
+            
+            if(nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
