4132f2deeaf465e14f73629d718e41187f09553e
Krystsina Kurytsyna <kuricyna20@Gmail.com>
2022-07-19 08:20:37 +0200

    Update removeOverlappingIntervals.java

---
+++ Intervals/removeOverlappingIntervals.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        int prevEnd = intervals[0][1];
+        int count = 0;
+        
+        for(int i = 1; i < intervals.length; i++) {
+            if(prevEnd > intervals[i][0]) {
+                count++;
+                prevEnd = Math.min(prevEnd, intervals[i][1]);
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
