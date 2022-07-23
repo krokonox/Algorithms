6dfcffd14b5d184e693c5d5b629213a6e332c076
Krystsina Kurytsyna <kuricyna20@Gmail.com>
2022-07-23 18:16:56 +0200

    Create LastWordLength.java

---
+++ String/LastWordLength.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int lastLen = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && lastLen > 0) {
+                return lastLen;
+            } else if (s.charAt(i) != ' ') {
+                lastLen++;
+            }
+        }
+        return lastLen;
+    }
+}
